2bfd130ded445c3a0bef77f09a597ebdda6c17a0
Hyunsoo, Chung <admin@hyonsu.com>
2022-03-04 23:23:38 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const uniqueString = require('unique-string');
+
+const waitingModal = {};
+
+module.exports = client => {
+    client.on('interactionCreate', interaction => {
+        if(interaction.isModalSubmit() && waitingModal[interaction.customId]) {
+            waitingModal[interaction.customId].resolve(interaction);
+            delete waitingModal[interaction.customId];
+        }
+
+        interaction.awaitModalSubmit = async (modal, time) => {
+            const customId = uniqueString();
+
+            modal.setCustomId(customId);
+
+            await interaction.showModal(modal);
+
+            const promise = new Promise((resolve, reject) => {
+                waitingModal[customId] = {
+                    resolve,
+                    reject
+                }
+            });
+
+            if(time) setTimeout(() => waitingModal[customId].reject(new Error('Modal timeout')), time);
+
+            return promise;
+        }
+    });
+}
